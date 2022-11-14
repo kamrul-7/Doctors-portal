2edeadbed2f801ab5341fc5c7aa95fbf523e1c7c
kamrul-7 <kamrul7.khan@gmail.com>
2022-11-14 12:04:36 +0600

    treatment

--- src/pages/Appointment/Appointment/BookingModal/BookingModal.js
+++ src/pages/Appointment/Appointment/BookingModal/BookingModal.js
@@ -3,6 +3,23 @@ import React from 'react';
 const BookingModal = ({ treatment, selectedDate }) => {
     const { name, slots } = treatment;
     const date = format(selectedDate, 'PP');
+    const handleBooking = event => {
+        event.preventDefault();
+        const form = event.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        const phone = form.phone.value;
+        const slot = form.slot.value;
+        const booking = {
+            appointmentDate: date,
+            treatment: name,
+            patient: name,
+            slot,
+            email,
+            phone,
+        }
+        console.log(booking)
+    }
     return (
         <>
             <input type="checkbox" id="booking-modal" className="modal-toggle" />
@@ -10,17 +27,28 @@ const BookingModal = ({ treatment, selectedDate }) => {
                 <div className="modal-box relative">
                     <label htmlFor="booking-modal" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
                     <h3 className="text-lg font-bold">{name}</h3>
-                    <form className='grid grid-cols-1 gap-3 mt-10'>
+                    <form onSubmit={handleBooking} className='grid grid-cols-1 gap-3 mt-10'>
                         <input type="text" disabled value={date} className="input input-bordered input-secondary w-full" />
-                        <select className="select select-info w-full">
+                        <select name='slot' className="select select-info w-full">
 
                             {
-                                slots.map(slot => <option value={slot}>{slot}</option>)
+                                slots.map((slot, index) => <option
+                                    value={slot}
+                                    key={index}
+                                >{slot}</option>)
                             }
 
                         </select>
-                        <input type="text" placeholder="Type here" className="input input-bordered input-secondary w-full" />
-                        <input type="text" placeholder="Type here" className="input input-bordered input-secondary w-full" />
+                        <select name='name' className="input input-bordered input-secondary w-full">
+                            {
+                                <option value={name}>{name}
+
+                                </option>
+                            }
+
+                        </select>
+                        <input name='email' type="email" placeholder="Your Email" className="input input-bordered input-secondary w-full" />
+                        <input name='phone' type="text" placeholder="Phone number" className="input input-bordered input-secondary w-full" />
                         <input className='btn btn-accent w-full ' type="submit" value="Submit" />
                     </form>
                 </div>
--- src/pages/Appointment/AvailableAppointments/AppointmentOption.js
+++ src/pages/Appointment/AvailableAppointments/AppointmentOption.js
@@ -9,7 +9,11 @@ const AppointmentOption = ({ option, setTreatment }) => {
                 <p>{slots.length > 0 ? slots[0] : 'Try another Day'}</p>
                 <p>{slots.length} {slots.length > 1 ? 'Spaces' : 'Space'} Available</p>
                 <div className='card-actions justify-center'>
-                    <label onClick={() => setTreatment(option)} htmlFor="booking-modal" className="btn btn-primary text-white">Book Appointment</label>
+                    <label
+                        disabled={slots.length === 0}
+                        onClick={() => setTreatment(option)}
+                        htmlFor="booking-modal" className="btn btn-primary text-white">Book Appointment
+                    </label>
                 </div>
             </div>
         </div>
